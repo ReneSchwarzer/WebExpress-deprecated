501577d4b74d71d1903429956464acedad943a3d
Rene Schwarzer <rene_schwarzer@hotmail.de>
2020-04-07 17:33:30 +0200

    Neues HTML-Element
    - Canvas

---
+++ core/WebExpress/Html/HtmlElementCanvas.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace WebServer.Html
+{
+    public class HtmlElementCanvas : HtmlElement
+    {
+        /// <summary>
+        /// Liefert die Elemente
+        /// </summary>
+        public new List<IHtmlNode> Elements => base.Elements;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public HtmlElementCanvas()
+            : base("canvas")
+        {
+
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="nodes">Der Inhalt</param>
+        public HtmlElementCanvas(params IHtmlNode[] nodes)
+            : this()
+        {
+            Elements.AddRange(nodes);
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="nodes">Der Inhalt</param>
+        public HtmlElementCanvas(IEnumerable<IHtmlNode> nodes)
+            : this()
+        {
+            base.Elements.AddRange(nodes);
+        }
+    }
+}
