5dc66bac10e5e95c82cabe355f322d4272dad211
Rene Schwarzer <rene_schwarzer@hotmail.de>
2021-07-22 18:17:05 +0200

    Icon Plug hinzugefügt

---
+++ core/WebExpress.WebApp/WebControl/ControlHeaderSettingsSystemInformation.cs
@@ -0,0 +1,47 @@
+﻿using WebExpress.Attribute;
+using WebExpress.Html;
+using WebExpress.Internationalization;
+using WebExpress.UI.Attribute;
+using WebExpress.UI.Component;
+using WebExpress.UI.WebControl;
+using WebExpress.WebApp.Components;
+
+namespace WebExpress.WebApp.WebControl
+{
+    [Section(Section.AppSettingsSecondary)]
+    //[Application("webexpress")]
+    public sealed class ControlHeaderSettingsSystemInformation : ControlDropdownItemLink, IComponent
+    {
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public ControlHeaderSettingsSystemInformation()
+            : base()
+        {
+            Init();
+        }
+
+        /// <summary>
+        /// Initialisierung
+        /// </summary>
+        private void Init()
+        {
+            TextColor = new PropertyColorText(TypeColorText.Dark);
+        }
+
+        /// <summary>
+        /// In HTML konvertieren
+        /// </summary>
+        /// <param name="context">Der Kontext, indem das Steuerelement dargestellt wird</param>
+        /// <returns>Das Control als HTML</returns>
+        public override IHtmlNode Render(RenderContext context)
+        {
+            Text = context.I18N("systeminformation.label");
+            Uri = context.Page.Uri.Root.Append("settings/systeminformation");
+            //Active = context.Page is IPageSettings ? TypeActive.Active : TypeActive.None;
+            Icon = new PropertyIcon(TypeIcon.InfoCircle);
+
+            return base.Render(context);
+        }
+    }
+}
---
+++ core/WebExpress.WebApp/WebResource/AdminPageSystemInformation.cs
@@ -0,0 +1,16 @@
+﻿using WebExpress.Attribute;
+
+namespace WebExpress.WebApp.WebResource
+{
+    [ID("SystemInformation")]
+    [Title("systeminformation.label")]
+    [Segment("systeminformation", "systeminformation.label")]
+    [Path("/settings")]
+    [Module("webexpress")]
+    [Context("admin")]
+    public sealed class AdminPageSystemInformation : PageTemplateWebApp
+    {
+
+    }
+}
+
