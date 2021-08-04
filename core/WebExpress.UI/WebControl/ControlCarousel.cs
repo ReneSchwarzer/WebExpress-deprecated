7f07178fc91bd6526d21b845e286561cfd7f14df
Rene Schwarzer <rene_schwarzer@hotmail.de>
2021-08-04 09:17:15 +0200

    Neues Steuerelement ControlCarousel

---
+++ core/WebExpress.UI/WebControl/ControlCarousel.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using WebExpress.Html;
+
+namespace WebExpress.UI.WebControl
+{
+    /// <summary>
+    /// Erstellt eine Slideshow
+    /// </summary>
+    public class ControlCarousel : Control
+    {
+        /// <summary>
+        /// Liefert oder setzt den Inhalt
+        /// </summary>
+        public List<ControlCarouselItem> Items { get; private set; } = new List<ControlCarouselItem>();
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="id">Die ID</param>
+        public ControlCarousel(string id = null)
+            : base(string.IsNullOrWhiteSpace(id) ? "carousel" : id)
+        {
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="items">Die Inhalte der Slideshow</param>
+        public ControlCarousel(params ControlCarouselItem[] items)
+            : this()
+        {
+            Items.AddRange(items);
+        }
+
+        /// <summary>
+        /// In HTML konvertieren
+        /// </summary>
+        /// <param name="context">Der Kontext, indem das Steuerelement dargestellt wird</param>
+        /// <returns>Das Control als HTML</returns>
+        public override IHtmlNode Render(RenderContext context)
+        {
+            // Indicators 
+            var indicators = new HtmlElementTextContentUl() { Class = "carousel-indicators" };
+            var index = 0;
+
+            foreach (var v in Items)
+            {
+                var i = new HtmlElementTextContentLi() { Class = index == 0 ? "active" : string.Empty };
+                i.AddUserAttribute("data-target", "#" + ID);
+                i.AddUserAttribute("data-slide-to", index.ToString());
+
+                indicators.Elements.Add(i);
+
+                index++;
+            }
+
+            index = 0;
+
+            // Ittems
+            var inner = new HtmlElementTextContentDiv() { Class = "carousel-inner" };
+            foreach (var v in Items)
+            {
+                var i = new HtmlElementTextContentDiv(v.Control.Render(context)) { Class = index == 0 ? "carousel-item active" : "carousel-item" };
+
+                if (!string.IsNullOrWhiteSpace(v.Headline) || !string.IsNullOrWhiteSpace(v.Text))
+                {
+                    var caption = new HtmlElementTextContentDiv
+                    (
+                        new HtmlElementSectionH3() { Text = v.Headline },
+                        new HtmlElementTextContentP() { Text = v.Text }
+                    ) 
+                    { 
+                        Class = "carousel-caption" 
+                    };
+                    
+                    i.Elements.Add(caption);
+                }
+
+                inner.Elements.Add(i);
+
+                index++;
+            }
+
+            // Navigationssteuerelemente
+            var navLeft = new HtmlElementTextSemanticsA(new HtmlElementTextSemanticsSpan() { Class = "carousel-control-prev-icon" })
+            {
+                Class = "carousel-control-prev",
+                Href = "#" + ID
+            };
+            navLeft.AddUserAttribute("data-slide", "prev");
+
+            var navRight = new HtmlElementTextSemanticsA(new HtmlElementTextSemanticsSpan() { Class = "carousel-control-next-icon" })
+            {
+                Class = "carousel-control-next",
+                Href = "#" + ID
+            };
+            navRight.AddUserAttribute("data-slide", "next");
+
+            var html = new HtmlElementTextContentDiv
+            (
+                indicators, inner, navLeft, navRight
+            )
+            {
+                ID = ID,
+                Class = Css.Concatenate("carousel slide", GetClasses()),
+                Style = GetStyles(),
+                Role = "carousel"
+            };
+
+            html.AddUserAttribute("data-ride", "carousel");
+
+            return html;
+        }
+    }
+}
---
+++ core/WebExpress.UI/WebControl/ControlCarouselItem.cs
@@ -0,0 +1,33 @@
+﻿using WebExpress.Html;
+
+namespace WebExpress.UI.WebControl
+{
+    /// <summary>
+    /// Erstellt eine Slideshowelement
+    /// </summary>
+    public class ControlCarouselItem
+    {
+        /// <summary>
+        /// Liefert oder setzt die Überschrift
+        /// </summary>
+        public string Headline { get; set; }
+
+        /// <summary>
+        /// Liefert oder setzt den Text
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Liefert oder setzt das Slideshowelement, wie zum Beispiel ein Bild
+        /// </summary>
+        public IControl Control { get; set; }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        public ControlCarouselItem()
+        {
+        }
+        
+    }
+}
